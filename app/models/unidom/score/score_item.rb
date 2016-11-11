051dd735d6a7b5a85f1f69b6e8d8021f67327731
Topbit Du <topbit.du@gmail.com>
2016-11-11 08:11:03 +0800

    1, Score Item model & migration.

---
+++ app/models/unidom/score/score_item.rb
@@ -0,0 +1,27 @@
+# Score Item 是评分项。
+# #template 是评分项模版。
+# #scorer 是评分者。
+# #score_keeper 是得分者。
+
+class Unidom::Score::ScoreItem < Unidom::Score::ApplicationRecord
+
+  self.table_name = 'unidom_score_items'
+
+  include Unidom::Common::Concerns::ModelExtension
+
+  validates :title, allow_blank: true, length: { in: 2..columns_hash['title'].limit }
+  validates :score, presence:    true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 1_000_000_000 }
+
+  belongs_to :sheet,    class_name:  'Unidom::Score::ScoreSheet',        foreign_key: :score_sheet_id
+  belongs_to :template, class_name:  'Unidom::Score::ScoreItemTemplate', foreign_key: :score_item_template_id
+  belongs_to :scorer,   polymorphic: true
+
+  scope :sheet_is,    ->(sheet)    { where sheet:       sheet           }
+  scope :template_is, ->(template) { where template_id: to_id(template) }
+  scope :scored_by,   ->(scorer)   { where scorer:      scorer          }
+
+  def self.score!(sheet: nil, scorer: nil, template: nil, name: template.try(:name), score: 0, scored_on: Date.current, opened_at: Time.now, description: nil, instruction: nil)
+    create! sheet: sheet, scorer: scorer, template: template, name: name, score: score, scored_on: scored_on, opened_at: opened_at, description: description, instruction: instruction
+  end
+
+end
---
+++ db/migrate/20060112000000_create_unidom_score_items.rb
@@ -0,0 +1,35 @@
+class CreateUnidomScoreItems < ActiveRecord::Migration
+
+  def change
+
+    create_table :unidom_score_items, id: :uuid do |t|
+
+      t.references :score_sheet,         type: :uuid, null: false
+      t.references :score_item_template, type: :uuid, null: true
+      t.references :scorer,              type: :uuid, null: false,
+        polymorphic: { null: false, default: '', limit: 200 }
+
+      t.string  :title,     null: true,  default: nil, limit:     200
+      t.decimal :score,     null: false, default: 0.0, precision: 12, scale: 2
+      t.date    :scored_on, null: false
+
+      t.text :description
+      t.text :instruction
+
+      t.column   :state, 'char(1)', null: false, default: 'C'
+      t.datetime :opened_at,        null: false, default: Time.utc(1970)
+      t.datetime :closed_at,        null: false, default: Time.utc(3000)
+      t.boolean  :defunct,          null: false, default: false
+      t.jsonb    :notation,         null: false, default: {}
+
+      t.timestamps null: false
+
+    end
+
+    add_index :unidom_score_sheets, :score_sheet_id
+    add_index :unidom_score_sheets, :score_item_template_id
+    add_index :unidom_score_sheets, :scorer_id
+
+  end
+
+end
