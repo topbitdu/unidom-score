f3c9cb81565db19481d6b702e9f10fd6e4e6f0f5
Topbit Du <topbit.du@gmail.com>
2016-11-11 18:18:06 +0800

    1, Score Sheet Template model & migration.

---
+++ app/models/unidom/score/score_sheet_template.rb
@@ -0,0 +1,29 @@
+# Score Sheet Tempalte 是评分表模版。
+# #template 是评分表模版。
+# #scorer 是评分者。
+# #score_keeper 是得分者。
+
+class Unidom::Score::ScoreSheetTemplate < Unidom::Score::ApplicationRecord
+
+  self.table_name = 'unidom_score_sheet_templates'
+
+  include Unidom::Common::Concerns::ModelExtension
+
+  validates :name,  allow_blank: true, length: { in: 2..columns_hash['name'].limit }
+  validates :score, presence:    true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 1_000_000_000 }
+
+  belongs_to :template,     class_name:  'Unidom::Score::ScoreSheetTemplate', foreign_key: :score_sheet_template_id
+  belongs_to :scorer,       polymorphic: true
+  belongs_to :score_keeper, polymorphic: true
+
+  has_many :score_items, class_name: 'Unidom::Score::ScoreItem'
+
+  scope :template_is,   ->(template)     { where template_id:  to_id(template) }
+  scope :scored_by,     ->(scorer)       { where scorer:       scorer          }
+  scope :score_kept_by, ->(score_keeper) { where score_keeper: score_keeper    }
+
+  def self.score!(scorer: nil, score_keeper: nil, template: nil, name: template.try(:name), score: 0, scored_on: Date.current, opened_at: Time.now, description: nil, instruction: nil)
+    create! scorer: scorer, score_keeper: score_keeper, template: template, name: name, score: score, scored_on: scored_on, opened_at: opened_at, description: description, instruction: instruction
+  end
+
+end
