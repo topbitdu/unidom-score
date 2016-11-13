0f3fccaf6fa790d70442538a941d5eae5093d421
Topbit Du <topbit.du@gmail.com>
2016-11-13 10:26:40 +0800

    1, Score Item Template model & migration.

---
+++ app/models/unidom/score/score_item_template.rb
@@ -0,0 +1,19 @@
+# Score Item Tempalte 是评分项模版。
+# #sheet 是评分表模版。
+
+class Unidom::Score::ScoreItemTemplate < Unidom::Score::ApplicationRecord
+
+  self.table_name = 'unidom_score_item_templates'
+
+  include Unidom::Common::Concerns::ModelExtension
+
+  validates :title, presence: true, length: { in: 2..columns_hash['title'].limit }
+  validates :score, presence: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 1_000_000_000 }
+
+  belongs_to :sheet, class_name: 'Unidom::Score::ScoreSheetTemplate'
+
+  has_many :scorings, class_name: 'Unidom::Score::ScoreItem', foreign_key: :template_id
+
+  scope :sheet_is, ->(sheet) { where sheet_id: to_id(sheet) }
+
+end
---
+++ db/migrate/20060102000000_create_unidom_score_item_templates.rb
@@ -0,0 +1,29 @@
+class CreateUnidomScoreItemTemplates < ActiveRecord::Migration
+
+  def change
+
+    create_table :unidom_score_item_templates, id: :uuid do |t|
+
+      t.references :sheet, type: :uuid, null: false
+
+      t.string  :title, null: false, default: '',  limit:     200
+      t.decimal :score, null: false, default: 0.0, precision: 12, scale: 2
+
+      t.text :description
+      t.text :instruction
+
+      t.column   :state, 'char(1)', null: false, default: 'C'
+      t.datetime :opened_at,        null: false, default: Time.utc(1970)
+      t.datetime :closed_at,        null: false, default: Time.utc(3000)
+      t.boolean  :defunct,          null: false, default: false
+      t.jsonb    :notation,         null: false, default: {}
+
+      t.timestamps null: false
+
+    end
+
+    add_index :unidom_score_item_templates, :sheet_id
+
+  end
+
+end
