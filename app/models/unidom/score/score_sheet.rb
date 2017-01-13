0c9de612eb78b366366d6f1d0c814e7d0b72adf2
Topbit Du <topbit.du@gmail.com>
2017-01-13 15:52:19 +0800

    1, Improve the Score Sheet model for the document.

--- app/models/unidom/score/score_sheet.rb
+++ app/models/unidom/score/score_sheet.rb
@@ -24,6 +24,11 @@ class Unidom::Score::ScoreSheet < Unidom::Score::ApplicationRecord
   scope :scored_by,     ->(scorer)       { where scorer:       scorer          }
   scope :score_kept_by, ->(score_keeper) { where score_keeper: score_keeper    }
 
+  ##
+  # 评分员 scorer 以 template 为模版（可以为空），对得分者 score_keeper 评分。评分表的名称为 name ，缺省为模版的名称。
+  # 得分为 score ，缺省为 0 。得分日期为 scored_on ，缺省为当天。记录时间为 opened_at ，缺省为当前时间。
+  # 另有适用于己方的备注 description 和适用于对方的备注 instruction。如：
+  # Unidom::Score::ScoreSheet.score! scorer: officer, score_keeper: shop, template: score_sheet_template, score: 100
   def self.score!(scorer: nil, score_keeper: nil, template: nil, name: template.try(:name), score: 0, scored_on: Date.current, opened_at: Time.now, description: nil, instruction: nil)
     create! scorer: scorer, score_keeper: score_keeper, template: template, name: name, score: score, scored_on: scored_on, opened_at: opened_at, description: description, instruction: instruction
   end
