304ae7b3b4d4c4b1a38f1730fb7c8364a51ad243
Topbit Du <topbit.du@gmail.com>
2017-02-01 19:10:58 +0800

    1, Models RSpec examples manifest.

---
+++ lib/unidom/score/models_rspec.rb
@@ -0,0 +1,4 @@
+require 'rspec/models/unidom/score/score_sheet_template_spec' unless Unidom::Common::Neglection.namespace_neglected? 'Unidom::Score::ScoreSheetTemplate'
+require 'rspec/models/unidom/score/score_item_template_spec'  unless Unidom::Common::Neglection.namespace_neglected? 'Unidom::Score::ScoreItemTemplate'
+require 'rspec/models/unidom/score/score_sheet_spec'          unless Unidom::Common::Neglection.namespace_neglected? 'Unidom::Score::ScoreSheet'
+require 'rspec/models/unidom/score/score_item_spec'           unless Unidom::Common::Neglection.namespace_neglected? 'Unidom::Score::ScoreItem'
