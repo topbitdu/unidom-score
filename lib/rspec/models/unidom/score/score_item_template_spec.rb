e495a8fca8c4fa6f93025dffc6ba213cd5754144
Topbit Du <topbit.du@gmail.com>
2017-03-07 12:06:39 +0800

    1, Improve the Score Item Template spec for the ``belongs_to :sheet, class_name: 'Unidom::Score::ScoreSheetTemplate'`` macro.

--- lib/rspec/models/unidom/score/score_item_template_spec.rb
+++ lib/rspec/models/unidom/score/score_item_template_spec.rb
@@ -16,6 +16,15 @@ describe Unidom::Score::ScoreItemTemplate, type: :model do
 
     it_behaves_like 'Unidom::Common::Concerns::ModelExtension', model_attributes
 
+    score_sheet_template_attributes = {
+      subject_id:    SecureRandom.uuid,
+      subject_type:  'Unidom::Score::Subject::Mock',
+      name:          'Some Name',
+      score:         100.00
+    }
+
+    it_behaves_like 'belongs_to', model_attributes, :sheet, Unidom::Score::ScoreSheetTemplate, score_sheet_template_attributes
+
   end
 
 end
