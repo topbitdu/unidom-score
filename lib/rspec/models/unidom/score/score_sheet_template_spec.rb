0208e758f77f2cd94d02b783958129ede22275f7
Topbit Du <topbit.du@gmail.com>
2017-03-27 21:21:59 +0800

    1, Improve the Score Sheet Template spec for the validations on the #name attribute, & the #score attribute.

--- lib/rspec/models/unidom/score/score_sheet_template_spec.rb
+++ lib/rspec/models/unidom/score/score_sheet_template_spec.rb
@@ -17,6 +17,12 @@ describe Unidom::Score::ScoreSheetTemplate, type: :model do
 
     it_behaves_like 'Unidom::Common::Concerns::ModelExtension', model_attributes
 
+    it_behaves_like 'validates text', model_attributes, :name,
+      length: 2..described_class.columns_hash['name'].limit
+
+    it_behaves_like 'validates numericality', model_attributes, :score,
+      range: -1_000_000_000..1_000_000_000, minimum_inclusive: true, maximum_inclusive: true, only_integer: false
+
   end
 
 end
